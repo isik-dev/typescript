d6e245d2945e784b870da5f28a434e22d3862381
isik <isik@finset.io>
2022-04-09 22:21:30 +0900

    [day-7] recap

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -4,4 +4,10 @@ import { NumbersCollection } from './NumbersCollection';
 const testArray = new NumbersCollection([0, 1, 1241, 1623, -213, 919]);
 const sorter = new Sorter(testArray);
 sorter.sort();
-console.log(testArray.data);
+// console.log(testArray.data);
+
+const shouldCheck = new NumbersCollection([-1.2, 3, 0, 0.9, -0.9, 24]);
+const sorted = new Sorter(shouldCheck);
+console.log(sorted);
+sorted.sort();
+console.log(shouldCheck.data);
